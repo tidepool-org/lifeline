3c19ef13f444b6678e891a96050af212063eb940
Jana E. Beck <jana.eliz.beck@gmail.com>
2014-08-16 16:46:24 -0700

    add month names

--- src/YearView/YearChart.js
+++ src/YearView/YearChart.js
@@ -9,19 +9,27 @@ d3.chart('Year', {
     this.w = this.base.attr('width');
     this.h = this.base.attr('height');
     this.emitter = new EventEmitter();
-    this.layer('month-boxes', this.base.append('g'), {
+
+    var chart = this;
+    var margin = 10;
+    var boxWidth = (this.w - margin * 7)/6, boxHeight = (this.h - margin * 3)/2;
+
+    this.layer('month-boxes', this.base.append('g').attr('id', 'month-boxes'), {
       dataBind: function() {
-        var chart = this.chart();
         return this.selectAll('rect').data(chart.months());
       },
       insert: function() {
         return this.append('rect');
       },
       events: {
-        'enter': function() {
-          var chart = this.chart();
-          var margin = 10;
-          var boxWidth = (chart.w - margin * 7)/6, boxHeight = (chart.h - margin * 3)/2;
+        enter: function() {
+          var xPosition = function(d, i) {
+            var month = d.month();
+            if (month >= 6) {
+              i = i - 6;
+            }
+            return i * boxWidth + (i + 1) * margin;
+          };
           var yPosition = function(d) {
             var month = d.month();
             if (month >= 6) {
@@ -30,13 +38,6 @@ d3.chart('Year', {
             else {
               return margin;
             }
-          };
-          var xPosition = function(d, i) {
-            var month = d.month();
-            if (month >= 6) {
-              i = i - 6;
-            }
-            return i * boxWidth + (i + 1) * margin;
           };
           this.attr({
             width: boxWidth,
@@ -55,8 +56,46 @@ d3.chart('Year', {
         }
       }
     });
+
+    this.layer('month-text', this.base.append('g').attr('id', 'month-text'), {
+      dataBind: function() {
+        return this.selectAll('text').data(chart.months());
+      },
+      insert: function() {
+        return this.append('text');
+      },
+      events: {
+        enter: function() {
+          var xPosition = function(d, i) {
+            var month = d.month();
+            if (month >= 6) {
+              i = i - 6;
+            }
+            return i * boxWidth + (i + 1) * margin + boxWidth/2;
+          };
+          var yPosition = function(d) {
+            var month = d.month();
+            if (month >= 6) {
+              return chart.h/2 + margin/2 + margin*2;
+            }
+            else {
+              return margin * 3;
+            }
+          };
+          var monthText = function(d) {
+            return d.format('MMMM');
+          }
+          this.attr({
+            x: xPosition,
+            y: yPosition,
+            'class': 'Chart-text Chart-text--centered'
+          })
+          .text(monthText);
+        }
+      }
+    });
   },
-  domain: function(year) {
+  location: function(year) {
     var _months = [];
     for (var i = 0; i < 12; ++i) {
       _months.push(moment(year + ' ' + (i + 1), 'YYYY M'));
@@ -79,7 +118,7 @@ module.exports = {
         viewBox: '0 0 ' + el.offsetWidth + ' ' + el.offsetHeight
       })
       .chart('Year')
-      .domain(options.domain);
+      .location(options.domain);
     return chart;
   }
 };
